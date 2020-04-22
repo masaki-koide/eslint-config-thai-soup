343a2e27a8416647c28e121a0f46e2f514f32d28
masaki-koide <ikasama0324@gmail.com>
2020-04-22 23:43:25 +0900

    chore: override plugin-import rules

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 module.exports = {
   extends: ['airbnb', 'airbnb/hooks', './base.js', 'prettier/react'],
   rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
     'import/order': ['error', { 'newlines-between': 'always' }],
+    'import/prefer-default-export': 'off',
     'react/button-has-type': 'off',
     'react/jsx-key': 'error',
     'react/jsx-props-no-spreading': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+      },
+    },
   },
 }
